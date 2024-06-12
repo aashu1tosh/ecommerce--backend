5e98f9c963c47788d78783d7f7dbfa55ab64a724
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-12 10:14:03 +0545

    chore: made print class for better console logging in dev and logging in a seprate folder in production

---
+++ src/utils/print.ts
@@ -0,0 +1,36 @@
+import chalk from "chalk";
+import { DotenvConfig } from "../config/env.config";
+import { Logger } from "../config/logger.config";
+import { Environment } from "../constant/enum";
+
+const log = console.log
+
+class Print {
+    static error(message: string): void {
+        if (DotenvConfig.NODE_ENV === Environment.DEVELOPMENT) log(chalk.bgRed(message));
+        else Logger.error(message);
+    }
+
+    static info(message: string): void {
+        if (DotenvConfig.NODE_ENV === Environment.DEVELOPMENT) log(chalk.green(message))
+        else Logger.info(message)
+    }
+
+    static warn(message: string): void {
+        if (DotenvConfig.NODE_ENV === Environment.DEVELOPMENT) log(chalk.yellow(message))
+        else Logger.warn(message)
+    }
+
+    static debug(message: string): void {
+        if (DotenvConfig.NODE_ENV === Environment.DEVELOPMENT) log(chalk.blue(message))
+        else Logger.debug(message)
+    }
+
+
+}
+
+export default Print
+
+
+
+
