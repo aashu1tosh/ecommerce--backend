1a119df4408341387bcc7ae1029e54825ebc99bb
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-12 10:14:48 +0545

    initialized the database and ran the server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
-import express, {Request, Response} from 'express'
-const cors = require('cors')
+import { createServer } from 'http';
+import app from './config/app.config';
+import { AppDataSource } from './config/database.config';
+import { DotenvConfig } from './config/env.config';
+import Print from './utils/print';
 
-const app = express();
+function listen() {
+  const PORT = DotenvConfig.PORT;
+  const httpServer = createServer(app)
+  httpServer.listen(PORT)
+  Print.info(`🚀 Server is listening on port ${PORT}`)
+}
 
-app.use(express.json());
-app.use(cors());
-
-app.listen(6000, () => {
-    console.log(`Server is running on http://localhost:6000`);
-});
+AppDataSource.initialize()
+  .then(async () => {
+    Print.info(`🚀 Database successfully connected`)
+    listen() // Start the server
+  })
+  .catch((err) => {
+    Print.info(`❌ Database connection failure - ${err?.message}`)
+  })
