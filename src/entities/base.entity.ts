90e961cb062c83d150f5a28dfffd0fb3ddc85dff
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-15 19:37:32 +0545

    chore: created a base entity with id, createdAt, updatedAt, deleteAt

---
+++ src/entities/base.entity.ts
@@ -0,0 +1,23 @@
+import { BaseEntity, BeforeInsert, Column, CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm'
+import createUUID from '../utils/uuid.utils'
+
+abstract class Base extends BaseEntity {
+  @Column({ primary: true, type: 'uuid' })
+  id: string | undefined
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date | undefined
+
+  @UpdateDateColumn({ name: 'update_at', select: false })
+  updatedAt: Date | undefined
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date | undefined
+
+  @BeforeInsert()
+  async UUID() {
+    this.id = await createUUID()
+  }
+}
+
+export default Base
