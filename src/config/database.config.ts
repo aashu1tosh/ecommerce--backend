b36984a7925323fcbbdd8f2c284fb97dd9bf4734
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-15 19:31:25 +0545

    add: to code to make sure entities work

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,4 +9,12 @@ export const AppDataSource = new DataSource({
   username: DotenvConfig.DATABASE_USERNAME,
   password: DotenvConfig.DATABASE_PASSWORD,
   database: DotenvConfig.DATABASE_NAME,
+  logging: false,
+  dropSchema: false,
+  synchronize: true,
+  // eslint-disable-next-line n/no-path-concat
+  entities: [__dirname + '/../entities/**/*.entity{.ts,.js}'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: [],
 })
+
