fb29c5aa9fdd9f9b0cb2cd0cfa92d9bd12322dbc
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-17 09:11:12 +0545

    wip: added bcrpt service for hashing and comparing

---
+++ src/services/bcrypt.service.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt'
+
+class BcryptService {
+    async hash(password: string): Promise<string> {
+        return await bcrypt.hash(password, 10);  
+    }
+
+    async compare(password: string, hash: string): Promise<boolean>{
+        return await bcrypt.compare(password, hash);
+    }
+}
+
+export default BcryptService
