021deb106d828e427559c1a897dd7ee57a6eda0f
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-18 16:35:33 +0545

    wip: created class for generating  webtokens

---
+++ src/services/webtoken.service.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { DotenvConfig } from '../config/env.config';
+
+class WebTokenServices {
+
+    sign(id: string) {
+        const token = jwt.sign(
+            { id },
+            DotenvConfig.JWT_SECRET as string,
+            { expiresIn: DotenvConfig.JWT_TOKEN_EXPIRE }
+        );
+        return token
+    }
+
+    verify(token: string) {
+        return jwt.verify(token, DotenvConfig.JWT_SECRET as string);
+    }
+
+}
+
+export default new WebTokenServices();
