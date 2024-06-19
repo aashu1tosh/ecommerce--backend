1fc83dc889bbf1f0da4f02c4579dcc6537ede33c
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-19 12:26:09 +0545

    feat: reset password through admin works

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,25 +1,39 @@
 import { AppDataSource } from "../config/database.config";
+import { ResetPasswordDTO } from "../dto/admin.dto";
 import { Auth } from "../entities/auth/auth.entity";
+import BcryptService from "./bcrypt.service";
 
 class AdminService {
+  constructor(
+    private readonly AuthRepo = AppDataSource.getRepository(Auth),
+    private readonly bcrpytService = new BcryptService()
+  ) {}
 
-    constructor(
-        private readonly AuthRepo = AppDataSource.getRepository(Auth),
-    ) { }
+  async getAll() {
+    const response = await this.AuthRepo.createQueryBuilder("auth")
+      .select(["auth.id", "auth.name", "auth.email", "auth.phone", "auth.role"])
+      .where("auth.role != :role", { role: "ADMIN" })
+      .getMany();
+    return response;
+  }
 
-    async getAll() {
-        const response = await this.AuthRepo.createQueryBuilder("auth")
-            .select(["auth.id", "auth.name", "auth.email", "auth.phone", "auth.role"])
-            .where("auth.role != :role", { role: "ADMIN" })
-            .getMany();
-        return response;
-    }
+  async resetPassword(data: ResetPasswordDTO) {
+    console.log(data);
+    const hash = await this.bcrpytService.hash(data?.newPassword);
+    const response = await this.AuthRepo.createQueryBuilder()
+      .update(Auth)
+      .set({
+        password: hash,
+      })
+      .where("id = :id", { id: data.id })
+      .execute();
+    return response;
+  }
 
-    async resetPassword() {
-        const response = await null;
-        return response;
-    }
+  // async deleteUser(data: string) {
 
+  //   console.log(data)
+  // }
 }
 
-export default new AdminService()
+export default new AdminService();
