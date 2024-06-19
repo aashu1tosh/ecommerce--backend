e14db69827435e95acc385ca74517fa105aa8037
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-19 11:11:24 +0545

    added: role service added

---
+++ src/services/utils/roles.service.ts
@@ -0,0 +1,21 @@
+import { AppDataSource } from "../../config/database.config";
+import { Auth } from "../../entities/auth/auth.entity";
+
+class RoleService {
+
+    constructor(
+        private readonly AuthRepo = AppDataSource.getRepository(Auth),
+    ) { }
+
+    async getRole(id: string) {
+
+        const response = await this.AuthRepo.createQueryBuilder("auth")
+            .select(["auth.role",])
+            .where("auth.id = :id", { id: id })
+            .getOne();
+        // console.log(role);
+        return response?.role;
+    }
+}
+
+export default new RoleService()
