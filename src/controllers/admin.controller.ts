5d92ed84a14941b52b9cd52d79dc9efbf86fe3c5
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-19 12:30:04 +0545

    feat: reset password controller added

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -3,24 +3,33 @@ import { StatusCodes } from "../constant/statusCodes";
 import adminService from "../services/admin.service";
 
 class AdminController {
-    async getAll(req: Request, res: Response) {
-        const response = await adminService.getAll();
-        res.status(StatusCodes.ACCEPTED).json({
-            success: true,
-            message: "Fetch Successful",
-            main: { response }
-        })
-    }
+  async getAll(req: Request, res: Response) {
+    const response = await adminService.getAll();
+    res.status(StatusCodes.ACCEPTED).json({
+      success: true,
+      message: "Fetch Successful",
+      main: { response },
+    });
+  }
 
-    async resetPassword(req: Request, res: Response) {
-        const response = await adminService.resetPassword();
-        res.status(StatusCodes.ACCEPTED).json({
-            success: true,
-            message: "Password Reset Successful",
-            main: null
-        })
-    }
+  async resetPassword(req: Request, res: Response) {
+    await adminService.resetPassword(req.body);
+    res.status(StatusCodes.ACCEPTED).json({
+      success: true,
+      message: "Password Reset Successful",
+      main: {},
+    });
+  }
 
+  // async deleteUser(req: Request, res: Response) {
+  //   const id = req.params.id
+  //   await adminService.deleteUser(id)
+  //   res.status(StatusCodes.ACCEPTED).json({
+  //     success: true,
+  //     message: "User Deletion Successful",
+  //     main: {},
+  //   });
+  // }
 }
 
-export default AdminController
+export default AdminController;
