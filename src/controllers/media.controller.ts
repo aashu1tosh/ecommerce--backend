50f6711022af214ed50591da973cd9d461396607
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-24 17:05:29 +0545

    feat: added media controllerto add image

---
+++ src/controllers/media.controller.ts
@@ -0,0 +1,29 @@
+import { type Request, type Response } from 'express'
+import { Message } from '../constant/messages'
+import { StatusCodes } from '../constant/statusCodes'
+import { ImageInterface } from '../interface/media.interface'
+import mediaService from '../services/media.service'
+import HttpException from '../utils/HttpException.utils'
+
+class MediaController {
+    async create(req: Request, res: Response) {
+        if (req.file) {
+            const image: ImageInterface = {
+                filename: req.file.filename,
+                filepath: req.file.path
+            }
+            const response = await mediaService.addMedia(image);
+            const id = response.id
+            res.status(StatusCodes.CREATED).json({
+                status: true,
+                message: Message.created,
+                main: {
+                    mediaId: id
+                }
+            })
+        }
+        else throw HttpException.badRequest('Please upload a file')
+    }
+}
+
+export default new MediaController()
