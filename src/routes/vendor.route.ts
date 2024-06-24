f6c18bb3a6b38540290912e85bd98088a587e6f1
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-24 17:19:40 +0545

    style: code format checked

--- src/routes/vendor.route.ts
+++ src/routes/vendor.route.ts
@@ -6,9 +6,10 @@ import RequestValidator from '../middleware/Request.Validator';
 import { authentication } from '../middleware/authentication.middleware';
 import { authorization } from '../middleware/authorization.middleware';
 import { catchAsync } from '../utils/catchAsync.utils';
+import mediaController from '../controllers/media.controller';
+import uploadFile from '../utils/fileUploads';
 
 const router = express.Router();
-
 router.use(authentication());
 router.use(authorization([ROLE.VENDOR]));
 const vendorController = new VendorController();
@@ -18,6 +19,12 @@ router.post(
     '/item',
     RequestValidator.validate(PostItemDTO),
     catchAsync(vendorController.createItem)
+);
+
+router.post(
+    '/media-upload',
+    uploadFile.single('image'),
+    catchAsync(mediaController.create)
 );
 
 router.delete('/:id', catchAsync(vendorController.deleteItem));
