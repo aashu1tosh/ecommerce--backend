db3c111c6e6970bc8d7e2706c95fc394bbe3eb33
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-26 16:07:35 +0545

    feat: update password feature added to admin customer and vendor

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import AuthController from '../controllers/auth.controller';
-import { CreateUserDTO, GoogleLoginDTO, LoginUserDTO } from '../dto/auth.dto';
+import {
+    CreateUserDTO,
+    GoogleLoginDTO,
+    LoginUserDTO,
+    UpdatePasswordDTO,
+} from '../dto/auth.dto';
 import RequestValidator from '../middleware/Request.Validator';
 import { catchAsync } from '../utils/catchAsync.utils';
+import { authentication } from '../middleware/authentication.middleware';
 
 const router = express.Router();
 
@@ -24,6 +30,13 @@ router.post(
     '/google',
     RequestValidator.validate(GoogleLoginDTO),
     catchAsync(authController.googleLogin)
+);
+
+router.patch(
+    '/update-password',
+    RequestValidator.validate(UpdatePasswordDTO),
+    authentication(),
+    catchAsync(authController.updatePassword)
 );
 
 export default router;
