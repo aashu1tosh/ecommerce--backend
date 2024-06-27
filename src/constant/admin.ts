d2a67e4311ccd5ff32ad268e341550f48f892bb7
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-27 10:17:20 +0545

    chore: added data which are to be seeded

---
+++ src/constant/admin.ts
@@ -0,0 +1,9 @@
+import { ROLE } from "./enum";
+
+export const admins = {
+    name: 'Admin dada',
+    email: 'admin@admin.com',
+    phone: '943818516',
+    password: 'Admin@123',
+    role: ROLE.ADMIN,
+}
