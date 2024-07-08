b727c51fe279285c558d4cf12ad6e8c37df1fce9
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-08 17:47:38 +0545

    feat: rate limit feature added

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,14 +1,22 @@
 import cors from 'cors';
 import express, { Application } from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { DotenvConfig } from '../config/env.config';
 import routes from '../routes/index.route';
 import { errorHandler } from './errorHandler.middleware';
-import helmet from 'helmet';
 
 const middleware = (app: Application) => {
     const allowedOrigins = DotenvConfig.CORS_ORIGIN;
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per windowMs
+        message:
+            'Too many requests from this IP, please try again after 15 minutes.',
+        headers: true, // Send rate limit info in response headers
+    });
 
     app.use(
         cors({
@@ -22,6 +30,9 @@ const middleware = (app: Application) => {
     );
 
     app.use(helmet());
+
+    // Apply rate limiting middleware to all requests
+    app.use(limiter);
 
     app.use(
         express.json({
